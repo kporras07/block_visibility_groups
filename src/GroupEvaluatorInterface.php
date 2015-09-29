09d80f2830d1f1b58a4281d50dfd8631d1b05ad1
Ted Bowman <ted.git@tedbow.com>
2015-09-29 17:14:27 -0400

    create group evaluator service

---
+++ src/GroupEvaluatorInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\block_visibility_groups\GroupEvaluatorInterface.
+ */
+
+namespace Drupal\block_visibility_groups;
+
+
+/**
+ * Interface GroupEvaluatorInterface.
+ *
+ * @package Drupal\block_visibility_groups
+ */
+interface GroupEvaluatorInterface {
+
+
+}
