f25978fad55bafbd01184baf84a73abe60e53adc
Ted Bowman <ted.git@tedbow.com>
2015-11-20 00:59:46 +0000

    Issue #2608736: Create Visibility tests

--- src/Tests/BlockVisibilityGroupsTestBase.php
+++ src/Tests/BlockVisibilityGroupsTestBase.php
@@ -28,4 +28,43 @@ abstract class BlockVisibilityGroupsTestBase extends WebTestBase {
       'administer blocks',
     ]));
   }
+
+  /**
+   * @param $plugin_id
+   * @param $group_id
+   * @param array $settings
+   *
+   * @return \Drupal\block\Entity\Block
+   */
+  protected function placeBlockInGroup($plugin_id, $group_id, $settings = []) {
+    $settings['label_display'] = 'visible';
+    $settings['label'] = $this->randomMachineName();
+    $settings['visibility']['condition_group']['block_visibility_group'] = $group_id;
+    $block = $this->drupalPlaceBlock($plugin_id, $settings);
+    return $block;
+  }
+  protected function placeBlockInGroupUI($plugin_id, $group_id, $title) {
+
+    // Enable a standard block.
+    $default_theme = $this->config('system.theme')->get('default');
+    $edit = array(
+      'id' => strtolower($this->randomMachineName(8)),
+      'region' => 'sidebar_first',
+      'settings[label]' => $title,
+    );
+    $block_id = $edit['id'];
+    if ($group_id) {
+      $edit['visibility[condition_group][block_visibility_group]'] = $group_id;
+    }
+
+
+    $this->drupalGet('admin/structure/block/add/' . $plugin_id . '/' . $default_theme);
+
+    $this->drupalPostForm(NULL, $edit, t('Save block'));
+    $this->assertText('The block configuration has been saved.', 'Block was saved');
+
+    // Just for Debug message.
+    $this->drupalGet('admin/structure/block/manage/' . $edit['id']);
+    $this->drupalGet('admin/structure/block/block-visibility-group/' . $group_id);
+  }
 }
