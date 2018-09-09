597965dc192d959f04a80e69f87c297e72622018
benjamin melançon <ben@agaric.com>
2018-09-09 00:08:45 -0400

    Use new recommended short array syntax Issue #2881398 by Munavijayalakshmi and #2978255 by vinod_kc

--- src/Form/BlockVisibilityGroupDeleteForm.php
+++ src/Form/BlockVisibilityGroupDeleteForm.php
@@ -20,7 +20,7 @@ class BlockVisibilityGroupDeleteForm extends EntityConfirmFormBase {
    * {@inheritdoc}
    */
   public function getQuestion() {
-    return $this->t('Are you sure you want to delete %name?', array('%name' => $this->entity->label()));
+    return $this->t('Are you sure you want to delete %name?', ['%name' => $this->entity->label()]);
   }
 
   /**
